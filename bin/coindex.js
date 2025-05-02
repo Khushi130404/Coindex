4fef3f0cd912d05aea77ac9a08b1f6c6c855f12d
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-05-02 12:28:40 +0530

    Set Att

--- bin/coindex.js
+++ bin/coindex.js
@@ -1,1 +1,12 @@
-console.log('Hello Coindex!');
+console.log("Hello Coindex!");
+
+const { Command } = require("commander");
+const program = new Command();
+const pkg = require("../package.json");
+
+program
+  .name("coindex")
+  .version(pkg.version)
+  .description("CLI to get cryptocurrency prices")
+  .command('key','Manage API keys')
+  .parse(process.argv);
