ce9dc1c5fef362702b809f0de860719d60f366ba
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-05-02 12:56:28 +0530

    Command-Key

---
+++ command/key.js
@@ -0,0 +1,13 @@
+const key = {
+  set: () => {
+    console.log("Set API keys");
+  },
+  show: () => {
+    console.log("Show API keys");
+  },
+  remove: () => {
+    console.log("Remove API keys");
+  }
+};
+
+module.exports = key;
