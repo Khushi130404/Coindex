b2432c1aecb3788475909beefb9cc0afe9c08757
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-05-03 08:50:28 +0530

    Set Key

--- command/key.js
+++ command/key.js
@@ -1,13 +1,33 @@
+const inquirer = require("inquirer");
+const colors = require("colors");
+const KeyManager = require("../lib/KeyManager");
+
 const key = {
-  set: () => {
-    console.log("Set API keys");
+  async set() {
+    const keyManag = new KeyManager();
+    const input = await inquirer.prompt([
+      {
+        type: "input",
+        name: "key",
+        message:
+          "Enter your API key ".green +
+          "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC",
+        validate: isRequired,
+      },
+    ]);
+    const key = keyManag.setKey(input.key);
+    if (key) {
+      console.log("API key set successfully".blue);
+    } else {
+      console.log("Failed to set API key".red);
+    }
   },
   show: () => {
     console.log("Show API keys");
   },
   remove: () => {
     console.log("Remove API keys");
-  }
+  },
 };
 
 module.exports = key;
