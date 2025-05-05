414fc21da8be99a1ca3c2e90dde687326bc1f587
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-05-05 09:45:39 +0530

    Check API

--- command/check.js
+++ command/check.js
@@ -1,6 +1,19 @@
+const KeyManager = require("../lib/KeyManager");
+const CryptoAPI = require("../lib/CryptoAPI");
+
 const check = {
   price(cmd) {
-    console.log(cmd.coin,cmd.currency);
+    async function fetchPrice() {
+      try {
+        keyMan = new KeyManager();
+        const key = keyMan.getKey();
+        const api = new CryptoAPI(key);
+        const priceData = await api.getPriceData(cmd.coin, cmd.currency);
+        console.log(priceData);
+      } catch (error) {
+        console.error("Error fetching price data:", error.message);
+      }
+    }
   },
 };
 
