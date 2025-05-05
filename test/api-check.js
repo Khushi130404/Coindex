ab6422b95cee3456047e41cb8febb67abc487b92
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-05-05 09:23:57 +0530

    Diff Currency Test

--- test/api-check.js
+++ test/api-check.js
@@ -1,13 +1,15 @@
 const axios = require("axios");
 
 const url =
-  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr";
+  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr,usd,eur";
 
 axios
   .get(url)
   .then((response) => {
-    const rate = response.data.bitcoin.inr;
-    console.log(`1 BTC = ₹${rate}`);
+    const rates = response.data.bitcoin;
+    console.log(`1 BTC = ₹${rates.inr} (INR)`);
+    console.log(`1 BTC = $${rates.usd} (USD)`);
+    console.log(`1 BTC = €${rates.eur} (EUR)`);
   })
   .catch((error) => {
     console.error(
