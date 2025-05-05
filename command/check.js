dd6a615f9194de342ea5ff1c11b64a52d0b5623d
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-05-05 09:49:23 +0530

    Sync Error Solves

--- command/check.js
+++ command/check.js
@@ -2,17 +2,15 @@ const KeyManager = require("../lib/KeyManager");
 const CryptoAPI = require("../lib/CryptoAPI");
 
 const check = {
-  price(cmd) {
-    async function fetchPrice() {
-      try {
-        keyMan = new KeyManager();
-        const key = keyMan.getKey();
-        const api = new CryptoAPI(key);
-        const priceData = await api.getPriceData(cmd.coin, cmd.currency);
-        console.log(priceData);
-      } catch (error) {
-        console.error("Error fetching price data:", error.message);
-      }
+  async price(cmd) {
+    try {
+      keyMan = new KeyManager();
+      const key = keyMan.getKey();
+      const api = new CryptoAPI(key);
+      const priceData = await api.getPriceData(cmd.coin, cmd.currency);
+      console.log(priceData);
+    } catch (error) {
+      console.error("Error fetching price data:", error.message);
     }
   },
 };
