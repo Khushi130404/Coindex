5ee1909b3507439f1c5b1c758570ff59ed17b44c
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-05-05 09:24:16 +0530

    Coin Geko

--- test/api-check.js
+++ test/api-check.js
@@ -1,15 +1,23 @@
 const axios = require("axios");
 
 const url =
-  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr,usd,eur";
+  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana&vs_currencies=inr,usd,eur";
 
 axios
   .get(url)
   .then((response) => {
-    const rates = response.data.bitcoin;
-    console.log(`1 BTC = ₹${rates.inr} (INR)`);
-    console.log(`1 BTC = $${rates.usd} (USD)`);
-    console.log(`1 BTC = €${rates.eur} (EUR)`);
+    const data = response.data;
+
+    console.log("💰 Current Prices:");
+    console.log(
+      `1 BTC = ₹${data.bitcoin.inr}, $${data.bitcoin.usd}, €${data.bitcoin.eur}`
+    );
+    console.log(
+      `1 ETH = ₹${data.ethereum.inr}, $${data.ethereum.usd}, €${data.ethereum.eur}`
+    );
+    console.log(
+      `1 SOL = ₹${data.solana.inr}, $${data.solana.usd}, €${data.solana.eur}`
+    );
   })
   .catch((error) => {
     console.error(
